2ae5ddee569cc287cf248cd3bcfea3553b9b8432
Shubham Agarwal <shubhamagarwal92@gmail.com>
2020-02-18 12:31:08 +0000

    SA: adding data in setup

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version='0.1.0',
     description='Show gCO2eq emissions information with nvidia-smi.',
     url='https://github.com/kylemcdonald/nvidia-co2',
+    download_url='https://github.com/kylemcdonald/nvidia-co2.git',
     author='Kyle McDonald',
     license='MIT',
     packages=['nvidia_co2'],
+    package_data={'nvidia_co2': ['data/*.json']},
     install_requires=[
         'shapely',
         'geocoder'
